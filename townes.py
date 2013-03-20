c5b01cecb222415a0fc0ab0628b3ca2b5db82736
Blake Griffith <blake.a.griffith@gmail.com>
2013-03-20 13:02:52 -0500

    Implemented Runge-Kutta, need to integrate with the shooting method.

--- townes.py
+++ townes.py
@@ -7,49 +7,59 @@ The Townes uses the solution to the following ODE
 
 R'' + (1/r)*R' - R + R**3 = 0
 
-Here we let R' = V then solve the following system using eulers method:
+Here we let R' = V then solve the following system using RK4 and the 
+shooting method with:
 V' = R - V/r - R**3
 R' = V
 """
 
-def V_step(r, h, R_old, V_old):
-    return V_old + h*(R_old - V_old/r - R_old**3)
+def f_V(h, r, V, R):
+    return  -V/r + R - R**3
 
-def R_step(r, h, R_old, V_old):
-    return R_old + h*V_old
+def f_R(h, r, V, R):
+    return V 
 
-def symmetrize(r):
-    rev = r[::-1]
-    rev = -1*rev
-    rev = rev[:-1]
-    return numpy.concatenate((rev, r))
+def k1(h, r, V, R):
+    k1V = f_V(h, r, V, R)
+    k1R = f_R(h, r, V, R)
+    return k1V, k1R
 
-def solver(h, N, Ri=2.28**.5):
-    """
-    Solve the ODE using Euler's method.
-    h is distance, Ri is R @ r=0, N is number of steps. 
-    """
-    r   = numpy.arange(0., h*N, h) 
-    R   = numpy.array([0.]*N)
-    dR  = numpy.array(R)
+def k2(h, r, V, R, k1V, k1R):
+    k2V = f_V(h, r + .5*h, V + h*.5*k1V, R + h*.5*k1R)
+    k2R = f_R(h, r + .5*h, V + h*.5*k1V, R + h*.5*k1R)
+    return k2V, k2R
 
-    R[0]    = Ri
-    dR[0]   = 0
-    for i in range(1, N):
-        R[i]    = R[i-1] + h*dR[i-1]
-        dR[i]   = dR[i-1]  + h*(R[i-1] - dR[i-1]/r[i] - R[i-1]**3)
+def k3(h, r, V, R, k2V, k2R):
+    k3V = f_V(h, r + .5*h, V + h*.5*k2V, R + h*.5*k2R)
+    k3R = f_R(h, r + .5*h, V + h*.5*k2V, R + h*.5*k2R)
+    return k3V, k3R
 
-    # symmetrize R
-    R = symmetrize(R)
-    R = R**2
+def k4(h, r, V, R, k3V, k3R):
+    k4V = f_V(h, r + h, V + h*k3V, R + h*k3R)
+    k4R = f_R(h, r + h, V + h*k3V, R + h*k3R)
+    return k4V, k4R
+
+def step(h, r, V, R):
+    k1V, k1R = k1(h, r, V, R)
+    k2V, k2R = k2(h, r, V, R, k1V, k1R)
+    k3V, k3R = k3(h, r, V, R, k2V, k2R)
+    k4V, k4R = k4(h, r, V, R, k3V, k3R)
     
-    # symmetrize r
-    r = symmetrize(r)
-    return r, R
-    
+    new_V = V + (h/6.)*(k1V + 2*k2V + 2*k3V + k4V)
+    new_R = R + (h/6.)*(k1R + 2*k2R + 2*k3R + k4R)
+
+    return new_V, new_R
+
+def RK4(N, h, IC):
+    """
+    Implement RK4 with N number of steps of size h with the initial 
+    condition IC.
+    """
+    pass
+
+
 
 def fit(peak_x, peak_y):
-
     pass
 
 if __name__=='__main__':
